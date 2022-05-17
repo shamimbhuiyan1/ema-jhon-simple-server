cb0092e8c3eabb18e689c19ce8cd3981190879ce
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-17 13:25:54 +0600

    products created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.9bftr.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://dbJhon1:6Rq1cKb2oJ4uHCPS@cluster1.9bftr.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,6 +19,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    await client.connect();
+    const productCollection = client.db("emaJhon").collection("product");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
   } finally {
   }
 }
